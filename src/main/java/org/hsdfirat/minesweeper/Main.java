85ee021962a45822d3d7bf8a3450faa000cd6551
berat <musafg@outlook.com>
2024-08-04 19:49:05 +0300

    name changes

--- src/main/java/org/hsdfirat/minesweeper/Main.java
+++ src/main/java/org/hsdfirat/minesweeper/Main.java
@@ -5,9 +5,9 @@ import javax.swing.*;
 public class Main {
 
     public static void main(String[] args) {
-        OyunCercevesi oyun = new OyunCercevesi();
-        oyun.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        oyun.setVisible(true);
+        OyunCercevesi game = new OyunCercevesi();
+        game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        game.setVisible(true);
     }
 
 }
